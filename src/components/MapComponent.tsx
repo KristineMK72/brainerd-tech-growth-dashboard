55695340b13f21ff67cdc960341178b5f8ffa28a
Kristine Kahler <124746855+KristineMK72@users.noreply.github.com>
2026-02-02 15:37:22 -0600

    Update MapComponent.tsx

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,22 +1,26 @@
+'use client';
+
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-// Brainerd coords: 46.3580° N, 94.2008° W
-// Tech locations (from local data: Ascensus ~Brainerd, Growth Zone ~Nisswa, etc.)
-const techSpots = [
-  { position: [46.358, -94.201], name: 'Ascensus (Tech Services, 480 jobs)', info: 'Financial tech hub' },
-  { position: [46.371, -94.289], name: 'Growth Zone (Software, 96 jobs)', info: 'Association management tech' },
-  { position: [46.352, -94.188], name: 'CTC (Telecom, 60 jobs)', info: 'Communications tech' },
-  { position: [46.355, -94.195], name: 'Marco Technologies', info: 'Managed IT services' },
-  // Add more or fetch dynamically
+// Center on Brainerd
+const center: [number, number] = [46.358, -94.201];
+
+const locations = [
+  { position: [46.358, -94.201] as [number, number], name: 'Brainerd – Ascensus & other tech services' },
+  { position: [46.471, -94.289] as [number, number], name: 'Nisswa / Pequot Lakes area' },
+  { position: [46.352, -94.188] as [number, number], name: 'Downtown Brainerd / Central Lakes College' },
 ];
 
 export default function MapComponent() {
   return (
-    <MapContainer center={[46.358, -94.201]} zoom={12} style={{ height: '100%', width: '100%' }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {techSpots.map((spot, i) => (
-        <Marker key={i} position={spot.position}>
-          <Popup>{spot.name}<br />{spot.info}</Popup>
+    <MapContainer center={center} zoom={11} style={{ height: '100%', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {locations.map((loc, i) => (
+        <Marker key={i} position={loc.position}>
+          <Popup>{loc.name}</Popup>
         </Marker>
       ))}
     </MapContainer>
